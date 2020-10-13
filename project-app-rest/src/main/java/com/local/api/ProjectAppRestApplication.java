f19cfaf832d4fd3d56cb27da2aeb0c26904d9413
Alex Amorim <obr.alexalexamorim@gmail.com>
2020-10-13 03:12:50 -0300

    projeto criado

---
+++ project-app-rest/src/main/java/com/local/api/ProjectAppRestApplication.java
@@ -0,0 +1,13 @@
+package com.local.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAppRestApplication.class, args);
+	}
+
+}
