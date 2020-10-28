ad083dde4545e5fe0b21c68bab1ae0ef07055598
Alex Amorim <obr.alexalexamorim@gmail.com>
2020-10-27 23:16:06 -0300

    alterado os nomes da entidades com @table

--- project-app-rest/src/main/java/com/local/api/model/entities/Produto.java
+++ project-app-rest/src/main/java/com/local/api/model/entities/Produto.java
@@ -8,9 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
+@Table(name = "tb_produto")
 public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
