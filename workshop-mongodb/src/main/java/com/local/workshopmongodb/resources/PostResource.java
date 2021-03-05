2c8bedf1ed369758228dd5690947f3c3502ab771
Alex Amorim <obr.alexalexamorim@gmail.com>
2021-03-04 23:09:48 -0300

    Consulta simples com query methods

--- workshop-mongodb/src/main/java/com/local/workshopmongodb/resources/PostResource.java
+++ workshop-mongodb/src/main/java/com/local/workshopmongodb/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.local.workshopmongodb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.local.workshopmongodb.domain.Post;
+import com.local.workshopmongodb.resources.uti.URL;
 import com.local.workshopmongodb.services.PostService;
 
 @RestController
@@ -21,6 +25,13 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = this.postService.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@RequestMapping(value = "/titulosearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitulo(@RequestParam(value="texto", defaultValue = "") String texto) {
+		texto = URL.decodeParam(texto);
+		List<Post> list = postService.findByTitulo(texto);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
--- workshop-mongodb/src/main/java/com/local/workshopmongodb/services/PostService.java
+++ workshop-mongodb/src/main/java/com/local/workshopmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.local.workshopmongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,10 @@ public class PostService {
 			throw new ObjectNotFoundException("Objeto nao encontrado");
 		}		
 		return post.get();
+	}
+	
+	public List<Post> findByTitulo(String texto){
+		return postRepository.findByTituloContainingIgnoreCase(texto);
 	}
 	
 }
